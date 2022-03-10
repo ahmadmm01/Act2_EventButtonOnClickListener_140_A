e4e71a0552cb301a85affb15b4d2e2e479e80150
ahmadmm01 <skyahmad74@gmail.com>
2022-03-10 22:26:46 +0700

    add intent and passing parameter

--- app/src/main/java/com/pam/activity2/MainActivity.java
+++ app/src/main/java/com/pam/activity2/MainActivity.java
@@ -2,6 +2,7 @@ package com.pam.activity2;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -12,7 +13,7 @@ public class MainActivity extends AppCompatActivity {
 
     Button btnLogin;
     EditText et_Email, et_Password;
-    String nama, password;
+    String nama, password, emailcheck, passcheck;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -33,20 +34,33 @@ public class MainActivity extends AppCompatActivity {
     public void validation(){
         nama = et_Email.getText().toString();
         password = et_Password.getText().toString();
+        emailcheck = "ahmadmm@gmail.com";
+        passcheck = "maddmm";
 
         if (nama.isEmpty() && password.isEmpty())
         {
             et_Email.setError("e-mail is required!");
             et_Password.setError("password is required!");
         } else {
-            if (nama.equals("ahmadmm@gmail.com") && password.equals("maddmm")) {
+            if (nama.equals(emailcheck) && password.equals(passcheck)) {
                 Toast t = Toast.makeText(getApplicationContext(),
                         "Successful Login! Your e-mail : " + nama + "  and Your password : " + password + "", Toast.LENGTH_LONG);
                 t.show();
-            } else if (!nama.equals("ahmadmm@gmail.com") && password.equals("maddmm")) {
+
+                Bundle b = new Bundle();
+
+                b.putString("a", nama.trim());
+                b.putString("b", password.trim());
+
+                Intent i = new Intent(getApplicationContext(), ActivityHasil.class);
+
+                i.putExtras(b);
+
+                startActivity(i);
+            } else if (!nama.equals(emailcheck) && password.equals(passcheck)) {
                 Toast t = Toast.makeText(getApplicationContext(),"Input the correct e-mail!", Toast.LENGTH_LONG);
                 t.show();
-            } else if (nama.equals("ahmadmm@gmail.com") && !password.equals("maddmm")) {
+            } else if (nama.equals(emailcheck) && !password.equals(passcheck)) {
                 Toast t = Toast.makeText(getApplicationContext(),"Input the correct password!", Toast.LENGTH_LONG);
                 t.show();
             } else {
